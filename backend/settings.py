ae233da911e5f97fc02636aa8d4d1753792f8589
Valentin Stanley <67529446+valen-xx@users.noreply.github.com>
2024-03-29 03:11:54 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -132,17 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'whoami/static')
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_ROOT =BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
