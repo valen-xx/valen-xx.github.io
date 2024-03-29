e4039be76c550e25713d7d38d50f04e7e019ff8e
valen-xx <67529446+valen-xx@users.noreply.github.com>
2024-03-29 01:45:50 -0400

    00

--- backend/settings.py
+++ backend/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = 'django-insecure-3iocly4f6@-jpkj=n8k@&_%t6qnbvuwo4@4+_x95nv0)e&38w#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+#ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
-ALLOWED_HOSTS = ['vava-nu.vercel.app']
+#ALLOWED_HOSTS = ['.vercel.app']
 #ALLOWED_HOSTS = []
 
 
