3bf6c9216639e384121646d9860d2c0f8632e16e
valen-xx <67529446+valen-xx@users.noreply.github.com>
2024-03-28 23:14:36 -0400

    try fixe static 000

--- vava/build_files.sh
+++ vava/build_files.sh
@@ -1,3 +1,5 @@
+pip install --root-user-action=ignore
+
 python3.9 -m pip install --upgrade pip
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic
