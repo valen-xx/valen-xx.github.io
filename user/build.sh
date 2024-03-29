0a6f89cb1f1a11bb9b1aff5cff3b43e564982197
valen-xx <67529446+valen-xx@users.noreply.github.com>
2024-03-28 23:40:36 -0400

    detelte sh 01

--- user/build.sh
+++ user/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
 
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 
-echo "Collect Static..."
+
+pip install --root-user-action=ignore
+
+python3.9 -m pip install --upgrade pip
+pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
