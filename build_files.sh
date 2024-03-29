2c8f0fa4e6a3170ad0d8d52b425d0bc30302ab0b
Valentin Stanley <67529446+valen-xx@users.noreply.github.com>
2024-03-29 02:47:19 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
-#pip3 install django
-#python3.9 manage.py collectstatic --noinput
-pip3 install -r requirements.txt
+# build_files.sh
+which python3.9
+pwd
+cd /usr/local/bin/
+yum install sqlite-devel -y
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+make && make altinstall
+cd /vercel/path0/
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
