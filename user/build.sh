f8c6d2c610007ced8715bf182ffe605d7115bddf
valen-xx <67529446+valen-xx@users.noreply.github.com>
2024-03-28 23:46:30 -0400

    01

--- user/build.sh
+++ user/build.sh
@@ -4,7 +4,7 @@
 
 
 pip install --root-user-action=ignore
-
+pip install django
 python3.9 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
